119b5aeabfe678bd3b561a24ad58509fc2224b3b
GIOQ777 <gioqitiashvili@yahoo.com>
2024-02-29 11:05:03 +0400

    calculator dakoreqtirda

--- calculator/main.js
+++ calculator/main.js
@@ -3,17 +3,22 @@
 //      b.	რიცხვების ვალიდირება
 //      c.	შედეგის წარმატებით გამოტანა, სწორი ინპუტების შემთხვევაშიjavasc
 
+debugger
 let num1 = prompt("შეიყვანეთ პირველი რიცხვი");
-while (isNaN(num1) || num1 === null) {
+while (isNaN(num1) || num1 === null || num1 === "") {
   alert("მონაცემი არასწორია, შეიყვანეთ რიცხვი");
   num1 = prompt("შეიყვანეთ პირველი რიცხვი");
-}
+};
 let operation = prompt("რომელი ოპერაციის განხორციელება გსურთ (+,-,*,/,%,**)");
+  while (operation !== `+` && operation !== `-` && operation !== `*` && operation !== `/` && operation !== `%` && operation !== `**`) {
+  alert(`ოპერატორი არასწორია, სცადეთ თავიდან (+,-,*,/,%,**)`);
+  operation = prompt("რომელი ოპერაციის განხორციელება გსურთ (+,-,*,/,%,**)");
+  };
 let num2 = prompt("შეიყვანეთ მეორე რიცხვი");
-while (isNaN(num2) || num2 === null) {
+while (isNaN(num2) || num2 === null || num1 === "") {
   alert("მონაცემი არასწორია, შეიყვანეთ რიცხვი");
   num2 = prompt("შეიყვანეთ მეორე რიცხვი");
-}
+};
 let result;
 num1 = Number(num1);
 num2 = Number(num2);
