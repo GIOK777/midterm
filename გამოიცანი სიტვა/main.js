57532160cbb5df0926a5cda38f802294dfb0dc2d
GIOQ777 <gioqitiashvili@yahoo.com>
2024-02-29 17:21:23 +0400

    gamoicani sitkva dakoreqtirda

--- გამოიცანი სიტვა/main.js
+++ გამოიცანი სიტვა/main.js
@@ -8,7 +8,6 @@
 const wordArr = [`ავტომობილი`, `ფურცელი`, `ადამიანი`, `ცხოველი`];
 console.log(wordArr);
 const randomElement = wordArr[Math.floor(Math.random() * wordArr.length)];
-console.log(` გამოსაცნობი სიტყვა - ${randomElement}`);
 
 const guessWord = [];
 for (let i = 0; i < randomElement.length; i++) {
@@ -18,21 +17,26 @@ console.log(...guessWord);
 
 let numberOfTries = Number(prompt(`შეიყვანეთ ცდების რაოდენობა`));
 console.log(`ცდების რაოდენობაა ${numberOfTries}`);
-for (let i = 1; i <= numberOfTries; i++) {
+for (let i = 1; i <= numberOfTries;) {
   let letter = prompt(`შეიყვანეთ ასო-ბგერა`);
 
-  for (let i = 0; i < randomElement.length; i++) {
-    if (randomElement[i] === letter) {
+  for (let j = 0; j < randomElement.length; j++) {
+    if (randomElement[j] === letter) {
       console.log(`შეიცავს (${letter}) - ს, გააგრძელე ცდა`);
-      guessWord[i] = letter;
+      guessWord[j] = letter;
     }
   }
 
   if (randomElement.indexOf(letter) === -1) {
-    console.log(`არ შეიცავს (${letter}) - ს, კიდევ სცადე`);
+    console.log(`არ შეიცავს (${letter}) - ს, დაგრჩა ${numberOfTries - i} ცდა`);
+            i++;
   }
 
   console.log(...guessWord);
+
+  if ( i > numberOfTries) {
+    console.log(` გამოსაცნობი სიტყვა - ${randomElement}`);
+  }
 }
 
 // for (let i = numberOfTries -1; i >= 0; i--){
